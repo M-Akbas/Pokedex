a40d3f051e36e34f4f0482f18acada8114e78bba
Muhammed Akbas <md.akbas@gmx.de>
2023-01-13 16:40:14 +0100

    for loop for card

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ let allPokemons = [];
 function init() {
   loadPokemons();
 }
+
+
+
 async function loadPokemons() {
   for (let i = 1; i < 20; i++) {
     // fetching 10 pokemons
@@ -11,9 +14,28 @@ async function loadPokemons() {
     let response = await fetch(url);
     let currentPokemon = await response.json();
     allPokemons.push(currentPokemon);
+    
   }
+  displayPokemon();
 }
 
+
 function displayPokemon() {
-    
+    for (let i = 1; i < allPokemons.length; i++) {
+      const pokemon = allPokemons[i];
+      let content = document.getElementById('content');
+      content.innerHTML += `
+      <div class="poke-card">
+        <h2 class="card-name">${pokemon['name']}</h2>
+        <div class="card-content">
+          <div class="background-color">
+            <img class="pokemon-img" src="${pokemon['sprites']['other']['official-artwork']['front_default']}"/>
+          </div>
+          <div class="types">
+            <p class="type-card">grass</p>
+            <p class="type-poison">poison</p>
+          </div>
+        </div>
+      </div>`;
+    }
 }
