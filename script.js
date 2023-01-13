b33ec915267c9fd5445cbe92a900c67ce2118e4a
Muhammed Akbas <md.akbas@gmx.de>
2023-01-13 01:37:55 +0100

    design

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-let currentPokemon; 
-let pokemons = [];
+let currentPokemon;
+let allPokemons = [];
 
+function init() {
+  loadPokemons();
+}
+async function loadPokemons() {
+  for (let i = 1; i < 20; i++) {
+    // fetching 10 pokemons
+    let url = `https://pokeapi.co/api/v2/pokemon/${i}/`;
+    let response = await fetch(url);
+    let currentPokemon = await response.json();
+    allPokemons.push(currentPokemon);
+  }
+}
 
-
-
- function init(){
-    loadPokemons();
+function displayPokemon() {
     
 }
-async function loadPokemons(){
-    for (let i = 1; i <= 10; i++) { // fetching 10 pokemons
-        let url = `https://pokeapi.co/api/v2/pokemon/${i}/`;
-        let response = await fetch(url);
-        let currentPokemon = await response.json();
-        pokemons.push(currentPokemon['name']);
-    }
-}
-
