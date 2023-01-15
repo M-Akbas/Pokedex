e51396c8abe431c1644c0c08f4c7f2a18fdddeff
Muhammed Akbas <md.akbas@gmx.de>
2023-01-15 19:09:18 +0100

    added design

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 let currentPokemon;
 let allPokemons = [];
+let color = '';
+
 
 function init() {
   loadPokemons();
 }
 
+
 async function loadPokemons() {
-  for (let i = 1; i < 20; i++) {
+  for (let i = 1; i < 153; i++) {
     // fetching 10 pokemons
     let url = `https://pokeapi.co/api/v2/pokemon/${i}/`;
     let response = await fetch(url);
@@ -15,6 +18,7 @@ async function loadPokemons() {
   }
   displayPokemon();
 }
+
 
 function getPokemonType(pokemon) {
   return pokemon["types"]["0"]["type"]["name"];
@@ -25,23 +29,30 @@ function getPokemonImage(pokemon) {
 function getPokemonName(pokemon){
   return pokemon["name"];
 }
+function getPokemonMove(pokemon){
+  return pokemon["moves"]["0"]["move"]["name"];
+}
+
+
 function displayPokemon() {
   for (let i = 1; i < allPokemons.length; i++) {
     const pokemon = allPokemons[i];
+    let color = getPokemonColor(pokemon);
     let content = document.getElementById("content");
     content.innerHTML += `
-      <div class="poke-card">
+      <div class="poke-card ${color}">
       <div class="d-FlexSpace">#${i}</div>
         <h2 class="card-name"> ${getPokemonName(pokemon)}</h2>
-        <div class="card-content">
-          <div class="background-color">
+        <div class="card-content ">
+          <div class="background-color ">
             <img class="pokemon-img" src="${getPokemonImage(pokemon)}"/>
           </div>
           <div class="types">
             <p class="type-card"> ${getPokemonType(pokemon)}</p>
-            <p class="type-move">${pokemon["moves"]["0"]["move"]["name"]}</p>
+            <p class="type-move">${getPokemonMove(pokemon)}</p>
           </div>
         </div>
       </div>`;
   }
+  getPokemonColor();
 }
---
+++ template.js
@@ -0,0 +1,32 @@
+function getPokemonColor(pokemon) {
+    let color = "";
+    let pokemonType = getPokemonType(pokemon);
+    if(pokemonType === "fire") {
+      color = "bg-fire";
+    } else if (pokemonType === "water") {
+      color = "bg-water";
+    } else if (pokemonType === "bug") {
+      color = "bg-bug";
+    } else if (pokemonType === "normal") {
+      color = "bg-normal";
+    } else if (pokemonType === "poison") {
+      color = "bg-poison";
+    } else if (pokemonType === "electric") {
+      color = "bg-electric";
+    } else if (pokemonType === "ground") {
+      color = "bg-ground";
+    } else if (pokemonType === "fairy") {
+      color = "bg-fairy";
+    } else if (pokemonType === "grass") {
+      color = "bg-grass";
+    } else if (pokemonType === "psychic") {
+      color = "bg-psychic";
+    } else if (pokemonType === "fighting") {
+      color = "bg-fighting";
+    } else if (pokemonType === "dragon") {
+      color = "bg-dragon";
+    } else if (pokemonType === "rock") {
+      color = "bg-rock";
+    } 
+    return color;
+  }
