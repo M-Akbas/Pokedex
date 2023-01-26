4dc5d7c01e836b23e172fcc67903d953a611623a
Muhammed Akbas <md.akbas@gmx.de>
2023-01-26 02:52:48 +0100

    dark/light mode implemented

--- script.js
+++ script.js
@@ -42,8 +42,11 @@ function getPokemonWeight(pokemon){
   return pokemon["weight"] / 10 ;
 }
 function getPokemonAbilities1(pokemon){
-  return pokemon['abilities']['0']['ability']['name'];
-
+  if(pokemon.hasOwnProperty('abilities') && pokemon.abilities.length > 1) {
+    return pokemon['abilities'][0]['ability']['name'];
+  } else {
+    return "";
+  }
 }
 function getPokemonAbilities2(pokemon){
   if(pokemon.hasOwnProperty('abilities') && pokemon.abilities.length > 1) {
@@ -85,19 +88,33 @@ function showPokemon(i) {
   let abilitie1 = getPokemonAbilities1(pokemon);
   let abilitie2 = getPokemonAbilities2(pokemon);
   pokedex.classList.add("overlay");
-  pokedex.innerHTML += `
+  pokedex.innerHTML = `
     <div class="pokedex-card ${color}">
-      <div class="d-FlexSpace">#${i + 1}</div>
-      <div class="card-name">${getPokemonName(pokemon)}</div>
-      <div class="typeO"><p class="type-cardP">${getPokemonType(pokemon)}</p><p class="type-moveP"> ${getPokemonMove(pokemon)}</p></div>
-      <div class="typeU"><p class="type-cardP">${getPokemonHeight(pokemon)} m</p><p class="type-moveP"> ${getPokemonWeight(pokemon)} kg</p></div>
-      <div class="d-FlexWidth">
-      <img class="arrowPng" src="img/arrowL.png"/><img class="arrowPng" src="img/arrowR.png"/>
+      <div class="d-FlexSpace">
+        #${i + 1}
       </div>
-      <div class="posAbs"><img class="pokemon-imgCard background-colorC ${borderColor}" src="${getPokemonImage(pokemon)}"/></div>
-      <div class="pokeData">
+      <div class="card-name">
+        ${getPokemonName(pokemon)}
+      </div>
+      <div class="typeO">
+        <p class="type-cardP">${getPokemonType(pokemon)}</p>
+        <p class="type-moveP"> ${getPokemonMove(pokemon)}</p>
+      </div>
+      <div class="typeU">
+        <p class="type-cardP">${getPokemonHeight(pokemon)} m</p>
+        <p class="type-moveP"> ${getPokemonWeight(pokemon)} kg</p>
+      </div>
+      <div class="d-FlexWidth">
+        <img onclick="lastPokemon(${i})" class="arrowPng" src="img/arrowL.png"/>
+        <img onclick="nextPokemon(${i})" class="arrowPng" src="img/arrowR.png"/>
+      </div>
+      <div class="posAbs">
+        <img id="pokeImg" class="pokemon-imgCard background-colorC ${borderColor}" src="${getPokemonImage(pokemon)}"/>
+      </div>
+      <div id="pokeData" class="pokeData">
        <div id="options" class="options ${textDeco}">
-        <p onclick="showPokemonStats()">STATS</p><p onclick="showPokemonMoves()">MOVES</p>
+        <p onclick="showPokemonStats()">STATS</p>
+        <p onclick="showPokemonMoves()">MOVES</p>
        </div>
        <div id="stats-section" class="abilites">
         <div class="abil-head">Abilities:</div>
@@ -105,15 +122,28 @@ function showPokemon(i) {
           <div>${abilitie1}</div>
           <div>${abilitie2}</div>
         </div>
-        <div>
-          <canvas id="myChart"></canvas>
-        </div>
        </div>
       </div>
     </div>
     
-  `;
+    `;
+    checkForColor();
 }
+
+function nextPokemon(i){
+  i++;
+  showPokemon(i);
+
+}
+function lastPokemon(i){
+  i--;
+  if(i <= 0){
+    alert('That the first Pokemon, go further, to see next ones!')
+  } else{
+    showPokemon(i);
+  }
+}
+
 function showPokemonMoves(){
   document.getElementById('stats-section').classList.add('d-none');
 }
@@ -125,11 +155,34 @@ function lightMode() {
   document.getElementById('body').classList.add('whiteBackground');
   document.getElementById('header').classList.add('whiteBackground');
   document.getElementById('header-font').classList.add('light-mode');
-  document.getElementById('button1').classList.add('border-color');
+  document.getElementById('button1').classList.add('border-color'); 
   bgColorForEachPoke();
 }
-
+function checkForColor(){
+  if(document.body.classList.contains("whiteBackground")){
+    document.getElementById('pokeData').classList.add('whiteBackground');
+    document.getElementById('pokeImg').classList.add('whiteBackground');
+    document.getElementById('options').classList.add('lightColor');
+    document.getElementById('stats-section').classList.add('lightColor');
+  } 
+}
+function darkMode(){
+  if(document.body.classList.contains("whiteBackground")){
+  document.getElementById('body').classList.remove('whiteBackground');
+  document.getElementById('header').classList.remove('whiteBackground');
+  document.getElementById('header-font').classList.remove('light-mode');
+  document.getElementById('button1').classList.remove('border-color'); 
+  bgColorForEachPokeDark();
+}}
 function bgColorForEachPoke(){
   for (let i = 0; i < allPokemons.length; i++) {
     document.getElementById(`background-of-pokemon${i}`).classList.add('whiteBackground');
+  
 }}
+function bgColorForEachPokeDark(){
+  for (let i = 0; i < allPokemons.length; i++) {
+    document.getElementById(`background-of-pokemon${i}`).classList.remove('whiteBackground');
+  
+}}
+
+
